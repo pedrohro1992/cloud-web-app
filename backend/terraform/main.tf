f26a124f5f0529383f012b2787412042333ecf10
Pedro Oliveira <pedro_hro1992@outlook.com>
2024-03-03 15:57:55 -0300

    Adiciona configuracao para backend

---
+++ backend/terraform/main.tf
@@ -0,0 +1,23 @@
+module "web_app" {
+  source = "../modules"
+
+  cluster_name = "cacetinho-sa-frontend"
+
+  instance_type = "t2.micro"
+
+  min_size = 1
+
+  max_size = 1
+
+  server_port = 80
+
+  ssh_server_port = 22
+
+  server_protocol = "TCP"
+
+  health_check_type = "EC2"
+
+}
+
+
+# TODO Mudar codigo do terraform para atender a maquina de backend
